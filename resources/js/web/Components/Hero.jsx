0dfa48dd8d706f7840d6937e23411c52ae86bc92
kimnopal <naufalhakim366@gmail.com>
2023-06-16 01:23:43 +0700

    menambahkan react dan memperbaiki view untuk react

---
+++ resources/js/web/Components/Hero.jsx
@@ -0,0 +1,18 @@
+const Hero = () => {
+    return (
+        <section className="px-4">
+            <div className="flex">
+                <div>
+                    <h1>Kamu merasa bingung?</h1>
+                    <h3>Ajukan pertanyaanmu disini</h3>
+                    <form action="">
+                        <input type="text" />
+                    </form>
+                </div>
+                <div></div>
+            </div>
+        </section>
+    );
+}
+
+export default Hero;
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,19 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite('resources/css/app.css')
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Document</title>
 </head>
+
 <body>
-  <h1 class="text-3xl font-bold underline text-cyan-500">
-    Hello world!
-  </h1>
+    <h1 class="text-3xl font-bold underline text-cyan-500">
+        Hello world!
+    </h1>
+
 </body>
+
 </html>
