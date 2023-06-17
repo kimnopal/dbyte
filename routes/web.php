edb3f702fa4baac6c34251a601974295ee33cd9a
Shafnaa <saujanashafi2003@gmail.com>
2023-06-17 21:38:59 +0700

    halaman login hampir jadi

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Home');
+    return Inertia::render('Auth/Login');
 });
 
 Route::get('/dashboard', function () {
