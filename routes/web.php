a3ac437734971bdc4054ad6cb2d76f93bb060c21
Shafnaa <saujanashafi2003@gmail.com>
2023-06-17 22:13:18 +0700

    halaman register hampir jadi

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Auth/Login');
+    return Inertia::render('Auth/Register');
 });
 
 Route::get('/dashboard', function () {
